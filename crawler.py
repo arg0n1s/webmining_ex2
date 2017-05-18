389bd6b5fdc786ff7801c2c4af7be55a8126c262
Arg0n1s <basti_seppl@yahoo.com>
2017-05-18 09:52:50 +0200

    crawling is now randomized, i think the performance might be a little worse but its still ok ..

--- crawler.py
+++ crawler.py
@@ -5,83 +5,62 @@ import pickle
 import codecs
 from threading import Thread
 import ssl
-import difflib
+import random
+import time
 
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+def start_crawl(initial_page, limit=300, numOfThreads=500):
 
-def start_crawl(initial_page, limit=100):
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
 
-    start = urllib.request.urlopen(initial_page, timeout=0.1, context=ctx)
-    soup = bs(start, 'lxml')
-
-    url_queue = {}
+    queue = []
     url_cache = {}
     url_out = {}
 
-    for link in soup.find_all('a'):
-        potential_link = link.get('href')
-        check = urllib.parse.urlparse(potential_link)
-        if check.scheme is '' or check.netloc is '':
-            potential_link = initial_page + potential_link
-        url_queue[potential_link] = potential_link
-    url_cache[initial_page]=soup
-    url_out[initial_page] = soup
+    queue.append(initial_page)
+
+    threads = []
 
     while url_out.__len__() < limit:
-        listOfQueues = []
-        threads = []
-        for item in url_queue:
-            if url_out.__len__() >= limit:
-                break;
-            t = Thread(target=crawl_thread, args=(item, listOfQueues, url_cache, url_out, limit))
+        active = []
+        for thread in threads:
+            if thread.is_alive():
+                active.append(thread)
+        threads = active
+        print("Num of Threads: ", threads.__len__(), "-->pre")
+        random.shuffle(queue)
+        while queue.__len__()>0 and threads.__len__()<=numOfThreads and url_out.__len__() < limit:
+            t = Thread(target=crawl_thread, args=(queue.pop(), queue, url_cache, url_out, limit, ctx))
             t.start()
             threads.append(t)
-        for thread in threads:
-            thread.join()
-        url_queue = {}
-        for next in listOfQueues:
-            if next[0] not in url_cache.keys() and next[0] not in url_queue.keys():
-                url_queue[next[0]]=next[0]
+        print("Num of Threads: ", threads.__len__(), "-->post")
+        print("Queue size: ",queue.__len__(), " Url Cache size: ",url_cache.__len__(), " Url out size: " ,url_out.__len__())
+        current = url_out.__len__()
+        while url_out.__len__()-current < 1:
+            time.sleep(0.1)
 
-    '''
-    url_out = {}
-    url_cache = list(url_cache.items())
-    while url_cache.__len__() > 0:
-        page = url_cache.pop(0)
-        isNew = True
-        if page[1] is None:
-            continue
-        str1 = page[1].prettify()
-        for item in url_cache:
-            if item[1] is None:
-                continue
-            str2 = item[1].prettify()
-            diff = difflib.SequenceMatcher(lambda x: x == " ", str1, str2)
-            if round(diff.ratio(), 3) > 0.6:
-                print(diff.ratio(), 3)
-                isNew = False
-                break
-        if isNew:
-            url_out[page[0]]=page[0]
-    '''
+    for thread in threads:
+        thread.join()
+
+    print("Queue size: ", queue.__len__(), " Url Cache size: ", url_cache.__len__(), " Url out size: ",
+          url_out.__len__())
     #pickle.dump(url_cache, open("crawl/result", "wb"))
     return url_out
 
-def crawl_thread(url, listOfQueues, url_cache, url_out, limit):
+def crawl_thread(url, queue, url_cache, url_out, limit, cert):
     crawled_links = {}
-    if url_out.__len__() >= limit:
-        return
+
     try:
-        page = urllib.request.urlopen(url, timeout=0.1, context=ctx)
+        page = urllib.request.urlopen(url, timeout=0.5, context=cert)
+
         s = bs(page, 'lxml')
         for link in s.find_all('a'):
             potential_link = link.get('href')
             check = urllib.parse.urlparse(potential_link)
             if check.scheme is '' or check.netloc is '':
                 potential_link = url + potential_link
-            if potential_link not in crawled_links.keys():
+            if potential_link not in crawled_links.keys() and potential_link not in url_cache.keys() and queue.__len__()<10000:
                 crawled_links[potential_link] = potential_link
                 #print(potential_link)
 
@@ -91,8 +70,8 @@ def crawl_thread(url, listOfQueues, url_cache, url_out, limit):
     except Exception as inst:
         #print(inst)
         url_cache[url] = None
-    listOfQueues.extend(list(crawled_links.items()))
-    print("Visited url: ", url, " Num:", url_out.__len__()," Visited: ", url_cache.__len__())
+    queue.extend(list(crawled_links.keys()))
+    #print("Visited url: ", url, " Num:", url_out.__len__()," Visited: ", url_cache.__len__())
 
 def save_crawl_as_html(db):
     #db = pickle.load( open( "crawl/result", "rb" ) )
@@ -120,6 +99,6 @@ def save_crawl_urls(db):
     file.write(urls)
     file.close()
 
-db = start_crawl("http://www.wikipedia.de", 1000)
-save_crawl_as_html(db)
+db = start_crawl("http://www.spiegel.de", 400, 250)
+#save_crawl_as_html(db)
 save_crawl_urls(db)
